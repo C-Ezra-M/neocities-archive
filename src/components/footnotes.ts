8b328c6b82eab333582637580031aeaf954f9cf6
C-Ezra-M <wmarzec06.wm@gmail.com>
2025-05-22 00:08:22 +0200

    further footnote work, add Error component for errors

--- src/components/footnotes.ts
+++ src/components/footnotes.ts
@@ -1,5 +1,15 @@
 import { atom, computed } from 'nanostores'
 
+/** The plain interface for footnote objects. It is functionally the same as `Footnote`, but has no methods. */
+export interface PlainFootnote {
+    /** The footnote's ID, which is slugged. The same as the key in the map. */
+    slug?: string;
+    /** The group name of the footnote. */
+    group?: string;
+    /** The content of the footnote. Its type is inferred as "any", since this plugin, like Astro itself, is meant to be framework-agnostic. */
+    content?: any;
+}
+
 /** The class for footnote objects. */
 export class Footnote {
     /** The footnote's ID, which is slugged. The same as the key in the map. */
@@ -7,16 +17,24 @@ export class Footnote {
     /** The group name of the footnote. */
     group?: string;
     /** The content of the footnote. Its type is inferred as "any", since this plugin, like Astro itself, is meant to be framework-agnostic. */
-    content: any;
+    content?: any;
 
-    constructor(settings: {slug: string, group?: string, content: any}) {
+    constructor(settings: PlainFootnote) {
         this.slug = settings.slug;
         this.group = settings.group;
         this.content = settings.content;
     }
 
+    hasContent() {
+        return !!this.content
+    }
+
+    hasGroup() {
+        return !!this.group
+    }
+
     hasSlug() {
-        return this.slug !== undefined || this.slug !== ""
+        return !!this.slug
     }
 
     htmlId() {
@@ -43,11 +61,23 @@ export function getFootnote(slug: string) {
 
 }
 
-export function footnote(fn: Footnote) {
-    // return a new footnote object if the slug doesn't exist, otherwise return that footnote
+export function footnote(fn: Footnote): Footnote {
+    // has slug only: get footnote from slug
+    // has group only: create fn in group
+    
+    // has slug and content: replace content and warn
+
+    if (!(fn instanceof Footnote)) {
+        fn = new Footnote(fn);
+    }
+    allFootnotes.get().find(e => e.slug === fn.slug)
+    // add to array and return a new footnote object if the slug doesn't exist, otherwise return that footnote
+    
     if (fn.hasSlug && allFootnotes.get().some(e => e.slug === fn.slug))
     console.warn(`Footnote with slug "${fn.slug}" found with new content. Only the last content provided will be used.`)
 }
+
+export function footnote(fn: PlainFootnote): Footnote;
 
 export function getFootnotesByGroup(gp: string) {
     return allFootnotes.get().filter(e => e.group === gp);
