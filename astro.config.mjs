4f7d17e064f80ce551d19be08c4b49e988313564
C-Ezra-M <wmarzec06.wm@gmail.com>
2025-06-14 20:56:00 +0200

    put all Markdown tables in a class so I wouldn't have to use HTML syntax

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import { addTableClass } from './remark-tables';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx()],
+    integrations: [mdx({
+        rehypePlugins: [addTableClass]
+    })],
 });
