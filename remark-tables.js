62ac16133c9cefb14a6d51e75ee290813c753da8
C-Ezra-M <wmarzec06.wm@gmail.com>
2025-07-31 01:05:07 +0200

    wrap tables in divs to allow horizontal scrolling

--- remark-tables.js
+++ remark-tables.js
@@ -3,10 +3,18 @@ import { visit } from 'unist-util-visit'
 
 export function addTableClass() {
     return function (tree) {
-        visit(tree, 'element', (node) => {
+        visit(tree, 'element', (node, index, parent) => {
             if (node.tagName === 'table') {
                 node.properties = node.properties || {};
                 node.properties.className = (node.properties.className || []).concat("table", "table-striped", "table-light");
+                const div = {
+                    type: 'element',
+                    tagName: 'div',
+                    properties: { className: "overflow-x-auto" },
+                    children: [node],
+                    position: node.position,
+                }
+                parent.children.splice(index, 1, div)
             }
         });
     };
