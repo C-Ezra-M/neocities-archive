2603dcb4c58a44a81e472512e3301cc524913d66
C-Ezra-M <wmarzec06.wm@gmail.com>
2025-07-31 01:08:20 +0200

    start blog

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob, file } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const blog = defineCollection({
+    loader: glob({ pattern: "**/*.mdx", base: "./src/blog" }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string().optional(),
+        date: z.coerce.date(),
+        updateDate: z.coerce.date().optional(),
+    })
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { blog };
