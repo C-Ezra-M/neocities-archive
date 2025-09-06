ba650fe040d5c45dd88005e1b9385b208d62e218
C-Ezra-M <wmarzec06.wm@gmail.com>
2025-09-06 20:04:51 +0200

    add themes, move CSS files around, add theme selector, theme page

---
+++ public/themes/mena.js
@@ -0,0 +1,37 @@
+
+{
+    const contents = $$("main > :not(h1:first-child)")
+    const header = $("main > h1:first-child")
+
+    $.create("div", { className: "page-content" })._.set({ after: header, contents })
+    //$.create("div", { className: "gap", after: $("nav .page-menu") })
+    $.create("dialog", {
+        id: "footer-dialog",
+        after: $("main"),
+        contents: [
+            $("footer"),
+            $.create("form", {
+                method: "dialog",
+                contents: [
+                    $.create("button", {
+                        contents: ["Close"]
+                    })
+                ]
+            }),
+        ]
+    })
+    $.create("li", {
+        contents: [
+            $.create("button", { 
+            events: {
+                click: (e) => {
+                    $("#footer-dialog").show()
+                }
+            },
+            contents: ["ℹ️"],
+            })
+        ],
+    after: $(".end-section > :last-child"),
+    })
+    
+}
