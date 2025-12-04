b91d65a1ac6603f063a4fa5d4d8b1f7fe91e555b
C-Ezra-M <wmarzec06.wm@gmail.com>
2025-12-05 00:38:41 +0100

    reorganize pages This is to make the content be in one place and the code in another.

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,15 +5,27 @@ import { defineCollection, z } from 'astro:content';
 import { glob, file } from 'astro/loaders';
 
 // 3. Define your collection(s)
-const blog = defineCollection({
-    loader: glob({ pattern: "**/*.mdx", base: "./src/blog" }),
+const content = defineCollection({
+    loader: glob({ pattern: ["**/*.mdx", "!blog/*"], base: "./content" }),
     schema: z.object({
         title: z.string(),
+        lang: z.string().default("en"),
         description: z.string().optional(),
-        date: z.coerce.date(),
+        date: z.coerce.date().optional(),
+        updateDate: z.coerce.date().optional(),
+    })
+});
+
+const blog = defineCollection({
+    loader: glob({ pattern: "**/*.mdx", base: "./content/blog" }),
+    schema: z.object({
+        title: z.string(),
+        lang: z.string().default("en"),
+        description: z.string().optional(),
+        date: z.coerce.date().optional(),
         updateDate: z.coerce.date().optional(),
     })
 });
 
 // 4. Export a single `collections` object to register your collection(s)
-export const collections = { blog };
+export const collections = { content, blog };
